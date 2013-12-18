fd05de5fa7025db78abc299815ed3cc6e22f8bac
Gordon Fontenot <gordon@thoughtbot.com>
2013-12-18 16:02:29 -0500

    Update version

--- git-update.rb
+++ git-update.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GitUpdate < Formula
   homepage 'https://github.com/gfontenot/git-update'
-  url 'https://github.com/gfontenot/git-update.git', :tag => '1.0'
+  url 'https://github.com/gfontenot/git-update.git', :tag => '1.0.1'
   head 'https://github.com/gfontenot/git-update.git'
 
   def install
