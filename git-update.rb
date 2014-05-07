7c6d753f8005087a5115f515316a0d1061c5c1c2
Gordon Fontenot <gordon@thoughtbot.com>
2014-05-07 16:59:43 -0400

    Update git-upate: 1.2

--- git-update.rb
+++ git-update.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GitUpdate < Formula
   homepage 'https://github.com/gfontenot/git-update'
-  url 'https://github.com/gfontenot/git-update.git', :tag => '1.1'
+  url 'https://github.com/gfontenot/git-update.git', :tag => '1.2'
   head 'https://github.com/gfontenot/git-update.git'
 
   def install
