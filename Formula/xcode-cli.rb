1992d7795890ae97171797da95ad4502904aeddc
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-04-09 09:22:58 -0500

    Update to newest version of xcode-cli

--- Formula/xcode-cli.rb
+++ Formula/xcode-cli.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class XcodeCli < Formula
   homepage "https://github.com/gfontenot/xcode-cli"
-  url "https://github.com/gfontenot/xcode-cli/archive/v1.1.0.tar.gz"
+  url "https://github.com/gfontenot/xcode-cli/archive/v1.2.0.tar.gz"
   sha256 "3fbda3266fe43cf300699542234090d9f05fd1edc1bb6e56b188e30a1f9f5656"
 
   depends_on "fzf"
